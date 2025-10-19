bcd943b24deb38da9b9102d062f8ce5301b921af
Matheus Fernandes <matheus.fernangomes@gmail.com>
2025-10-19 16:24:23 -0300

    feature: Add Application Dtos classes

---
+++ BarberBoss.Application/Dtos/BillingResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace BarberBoss.Application;
+
+public class BillingResponseDto
+{
+    public Guid Id { get; set; }
+    public DateOnly Date { get; set; }
+    public string BarberName { get; set; } = string.Empty;
+    public string ClientName { get; set; } = string.Empty;
+    public string ServiceName { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public string PaymentMethod { get; set; } = string.Empty; // Retornar como string
+    public string Status { get; set; } = string.Empty; // Retornar como string
+    public string? Notes { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ BarberBoss.Application/Dtos/UpdateBillingDto.cs
@@ -0,0 +1,29 @@
+﻿using BarberBoss.Domain;
+using BarberBoss.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BarberBoss.Application;
+
+public class UpdateBillingDto
+{
+    [Required]
+    public DateOnly Date { get; set; }
+    [Required]
+    [StringLength(80, MinimumLength = 2)]
+    public string BarberName { get; set; }
+    [Required]
+    [StringLength(120, MinimumLength = 2)]
+    public string ClientName { get; set; }
+    [Required]
+    [StringLength(120, MinimumLength = 2)]
+    public string ServiceName { get; set; }
+    [Required]
+    [Range(0.0, double.MaxValue)] // Pode ser 0 se for cancelado
+    public decimal Amount { get; set; }
+    [Required]
+    public PaymentMethod PaymentMethod { get; set; }
+    [Required]
+    public BillingStatus Status { get; set; }
+    [StringLength(500)]
+    public string? Notes { get; set; }
+}
